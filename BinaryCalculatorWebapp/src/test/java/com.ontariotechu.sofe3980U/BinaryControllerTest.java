c62ff24d7dc276ca3560305c580687754eeda285
DrewGlover05 <drewglover05@icloud.com>
2026-03-14 20:41:53 -0400

    Enhance BinaryControllerTest with new operations
    Added new test cases for addition, subtraction, multiplication, and bitwise operations in BinaryControllerTest.

--- BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
+++ BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
@@ -1,27 +1,16 @@
 package com.ontariotechu.sofe3980U;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
 import org.junit.runner.RunWith;
-
-import org.junit.*;
-import org.junit.runner.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.context.*;
-import org.springframework.boot.test.mock.mockito.*;
-import org.springframework.test.context.junit4.*;
-
-import static org.hamcrest.Matchers.containsString;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BinaryController.class)
@@ -56,5 +45,80 @@ public class BinaryControllerTest {
 			.andExpect(model().attribute("result", "1110"))
 			.andExpect(model().attribute("operand1", "111"));
     }
+    
+    /* 
+        New Test Cases for Addition
+    */ 
+    @Test
+    public void testAddWithZero() throws Exception {
+        mvc.perform(post("/")
+            .param("operand1", "0")
+            .param("operator", "+")
+            .param("operand2", "1010"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("result"))
+        .andExpect(model().attribute("result", "1010"));
+    }
 
+    @Test
+    public void testAddLargeNumbers() throws Exception {
+        mvc.perform(post("/")
+            .param("operand1", "11111111")
+            .param("operator", "+")
+            .param("operand2", "1"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("result"))
+        .andExpect(model().attribute("result", "100000000"));
+    }
+
+    @Test
+    public void testAddDifferentLengths() throws Exception {
+        mvc.perform(post("/")
+            .param("operand1", "1010")
+            .param("operator", "+")
+            .param("operand2", "11"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("result"))
+        .andExpect(model().attribute("result", "1101"));
+    }
+
+    /* 
+        New Test cases for Subtraction, Multiplication, and Division
+    */
+
+    // test or
+    @Test
+    public void testOr() throws Exception {
+        mvc.perform(post("/")
+            .param("operand1", "1010")
+            .param("operator", "|")
+            .param("operand2", "11"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("result"))
+        .andExpect(model().attribute("result", "1011"));
+    }
+
+    // test multiplication
+    @Test
+    public void testMultiply() throws Exception {
+        mvc.perform(post("/")
+            .param("operand1", "1010")
+            .param("operator", "*")
+            .param("operand2", "11"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("result"))
+        .andExpect(model().attribute("result", "11110"));
+    }
+
+    //test and
+    @Test
+    public void testAnd() throws Exception {
+        mvc.perform(post("/")
+            .param("operand1", "1010")
+            .param("operator", "&")
+            .param("operand2", "11"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("result"))
+        .andExpect(model().attribute("result", "10"));
+    }
 }
